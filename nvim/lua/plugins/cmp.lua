a0e23040a0bb84a21319b726fc8b848f494af658
Retr0327 <retr0327.dev@gmail.com>
2026-03-15 16:02:36 +0800

    fix: remove duplicate lspkind.nvim dependency in cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -37,7 +37,6 @@ return {
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp-signature-help",
-      "onsails/lspkind.nvim",
       {
         "L3MON4D3/LuaSnip",
         version = "v2.3",
