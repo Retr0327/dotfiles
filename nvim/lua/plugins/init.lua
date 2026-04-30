085a2aabe686c82288c4e101dea0e56f71eccdca
Retr0327 <retr0327.dev@gmail.com>
2026-05-01 01:44:59 +0800

    chore: reorganize nvim plugins imports

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  { import = "plugins.editor" },
+  { import = "plugins.lsp" },
+  { import = "plugins.ui" },
+  { import = "plugins.git" },
+  { import = "plugins.file" },
+  { import = "plugins.workflow" },
+}
