06e3d90df415198fc47623272aaa2d1c40892082
Retr0327 <retr0327.dev@gmail.com>
2026-05-01 13:36:29 +0800

    refactor: improve filename-based icon lookup

--- nvim/lua/plugins/ui/icon.lua
+++ nvim/lua/plugins/ui/icon.lua
@@ -2,31 +2,18 @@ return {
   {
     "nvim-tree/nvim-web-devicons",
     opts = {
-      override = {
-        js = {
-          icon = "",
-          color = "#ffca28",
-          name = "Js",
-        },
-        ts = {
-          icon = "",
-          color = "#0288d1",
-          name = "Ts",
-        },
-        md = {
-          icon = "󰍔",
-          color = "#42a5f5",
-          name = "Markdown",
-        },
-        go = {
-          icon = "󰟓",
-          color = "#00acc1",
-          name = "Go",
-        },
+      strict = true,
+      -- In strict mode, filename and extension overrides are used directly.
+      override_by_filename = {
         ["go.mod"] = {
           icon = "󰟓",
           color = "#ec407a",
           name = "GoModule",
+        },
+        [".gitignore"] = {
+          icon = "",
+          color = "#e64a19",
+          name = "GitIgnore",
         },
         ["package.json"] = {
           icon = "󰎙",
@@ -39,7 +26,7 @@ return {
           name = "PackageLockJson",
         },
         ["README.md"] = {
-          icon = "󰋼",
+          icon = "",
           color = "#42a5f5",
           name = "README",
         },
@@ -49,11 +36,63 @@ return {
           name = "SEQUELIZE",
         },
         ["Dockerfile"] = {
-          icon = "",
+          icon = "󰡨",
           color = "#0288D1",
           name = "DOCKERFILE",
         },
       },
+      override_by_extension = {
+        js = {
+          icon = "",
+          color = "#ffca28",
+          name = "Js",
+        },
+        ts = {
+          icon = "",
+          color = "#0288d1",
+          name = "Ts",
+        },
+        go = {
+          icon = "󰟓",
+          color = "#00acc1",
+          name = "Go",
+        },
+        java = {
+          icon = "󰅶",
+          color = "#F44336",
+          name = "Java",
+        },
+        class = {
+          icon = "󰅶",
+          color = "#1e88e5",
+          name = "JavaClass",
+        },
+        md = {
+          icon = "",
+          color = "#42a5f5",
+          name = "Markdown",
+        },
+      },
     },
+    config = function(_, opts)
+      local devicons = require("nvim-web-devicons")
+      devicons.setup(opts)
+      if not devicons._basename_lookup_patched then
+        local get_icon = devicons.get_icon
+        devicons.get_icon = function(name, ext, icon_opts)
+          if type(name) == "string" and name ~= "" then
+            local base = vim.fs.basename(name)
+            if base ~= "" then
+              name = base
+            end
+          end
+          if (not ext or ext == "") and type(name) == "string" then
+            ext = name:match("^.+%.([^.]+)$")
+          end
+          return get_icon(name, ext, icon_opts)
+        end
+        devicons._basename_lookup_patched = true
+      end
+    end,
   },
 }
